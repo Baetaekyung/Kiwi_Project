b08e196ea316a06a9cd12981b9a74cf74ea61dec
BaeTaeKyung <tkwayi2129@gmail.com>
2023-10-24 09:51:58 +0900

    Arena system
    ss

--- Assets/Scripts/ArenaOn.cs
+++ Assets/Scripts/ArenaOn.cs
@@ -8,26 +8,21 @@ public class ArenaOn : MonoBehaviour
     GameObject player;
     SpriteRenderer spriteRenderer;
     PlayerFighting pF;
-    CompositeCollider2D compositcollider;
+    Collider2D boxCollider;
 
-    float clampValueX = 0f;
-    float clampValueY = 0f;
-    float clampPosMinusX = 0;
-    float clampPosMinusY = 0;
-    float clampPosPlusX = 0;
-    float clampPosPlusY = 0;
+    Vector3 lastPos = Vector3.zero;
 
     private void Awake()
     {
         pF = FindObjectOfType<PlayerFighting>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        compositcollider = GetComponent<CompositeCollider2D>();
+        boxCollider = GetComponent<Collider2D>();
     }
 
     void Start()
     {
         spriteRenderer.enabled = false;
-        compositcollider.enabled = false;
+        boxCollider.enabled = false;
     }
 
     void Update()
@@ -45,25 +40,30 @@ public class ArenaOn : MonoBehaviour
         if (pF.isFighting)
         {
             spriteRenderer.enabled = true;
-            compositcollider.enabled = true;
+            boxCollider.enabled = true;
             //player.transform.position = SelectClampPos();
         }
         // Off
         if (pF.endFight)
         {
             spriteRenderer.enabled = false;
-            compositcollider.enabled = false;
+            boxCollider.enabled = false;
         }
     }
 
-    //private Vector2 SelectClampPos()
-    //{
-    //    clampPosMinusX = transform.position.x - boxCollider.composite.bounds.extents.x;
-    //    clampPosPlusX = transform.position.x + boxCollider.composite.bounds.extents.x;
-    //    clampPosMinusY = transform.position.y - boxCollider.composite.bounds.extents.y;
-    //    clampPosPlusY = transform.position.y + boxCollider.composite.bounds.extents.y;
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            lastPos = other.transform.position;
+        }
+    }
 
-    //    return new Vector2(Mathf.Clamp(clampValueX, clampPosMinusX, clampPosPlusX),
-    //        Mathf.Clamp(clampValueY, clampPosMinusY, clampPosPlusY));
-    //}
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            other.transform.position = lastPos;
+        }
+    }
 }
